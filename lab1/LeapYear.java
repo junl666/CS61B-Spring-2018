cf4d3bf9c231338891cdc1e13a05413f8459535b
junl <junl03@outlook.com>
2024-05-30 18:28:13 +0800

    Lab1, finished

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,22 @@ public class LeapYear {
         }
     }
 
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
